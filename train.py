0225a395f44446f9555623a878ba3daac9b88b0d
manhdung20112000 <manhdung20112000@gmail.com>
2021-04-16 13:21:17 +0700

    train demo

--- train.py
+++ train.py
@@ -1,54 +1,58 @@
-import pandas as pd 
-import numpy as np
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
-from sklearn import preprocessing
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
-from sklearn.model_selection import train_test_split
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
 import json
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
+from tqdm import tqdm
 
-df = pd.read_csv("data_processed.csv")
+from models.model import Net
+from models.datasets import attempt_dataload
 
-#### Get features ready to model! 
-y = df.pop("cons_general").to_numpy()
-y[y< 4] = 0
-y[y>= 4] = 1
+# get dataloaders
+batch_size = 4
+seed = 42
+trainloader, testloader = attempt_dataload(batch_size=batch_size, seed=42, download=False)
 
-X = df.to_numpy()
-X = preprocessing.scale(X) # Is standard
-# Impute NaNs
+# device 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+try:
+    print(f"Training on {torch.cuda.get_device_name(device)}")
+except:
+    print(f"Training on CPU")
 
-imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp.fit(X)
-X = imp.transform(X)
+# model
+net = Net().to(device)
 
 
-# Linear model
-clf = QuadraticDiscriminantAnalysis()
-yhat = cross_val_predict(clf, X, y, cv=5)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-acc = np.mean(yhat==y)
-tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
-specificity = tn / (tn+fp)
-sensitivity = tp / (tp + fn)
+progress_bar = tqdm(range(3))
 
-# Now print to file
-with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc, "specificity": specificity, "sensitivity":sensitivity}, outfile)
+for epoch in progress_bar:
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
 
-# Let's visualize within several slices of the dataset
-score = yhat == y
-score_int = [int(s) for s in score]
-df['pred_accuracy'] = score_int
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-# Bar plot by region
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-sns.set_color_codes("dark")
-ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette = "Greens_d")
-ax.set(xlabel="Region", ylabel = "Model accuracy")
-plt.savefig("by_region.png",dpi=80)
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            progress_bar.set_description('epoch: %d | i: %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# save model
+PATH = 'models/net.pth'
+torch.save(net.state_dict(), PATH)
